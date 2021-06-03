1e4dfadaac76eac5d6501600b3fdcdea6bc855e9
Kafva <Martensson.Jonas98@gmail.com>
2021-06-03 23:06:39 +0200

    Switched to scss

--- webpack.config.js
+++ webpack.config.js
@@ -22,8 +22,23 @@ module.exports = {
 
     module: {
         rules: [
-            { test: /\.tsx?$/, loader: "ts-loader" },
-            { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+            {
+                // TS(X) loader
+                test: /\.tsx?$/, 
+                loader: "ts-loader" 
+            },
+            {
+              // SCSS-loader
+              test: /\.s[ac]ss$/i,
+              use: [
+                // Creates `style` nodes from JS strings
+                "style-loader",
+                // Translates CSS into CommonJS
+                "css-loader",
+                // Compiles Sass to CSS
+                "sass-loader"
+              ],
+            },
         ]
     },
     plugins: [
